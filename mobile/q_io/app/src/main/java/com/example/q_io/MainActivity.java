a0842872d7eb6c806d391c02299f8025b416b4fa
Dmytro Lunhu <dlunhu@isi-mtl.com>
2020-11-17 12:09:30 -0500

    Login. Validation_Début. Page accueil d'employé_Début

--- mobile/q_io/app/src/main/java/com/example/q_io/MainActivity.java
+++ mobile/q_io/app/src/main/java/com/example/q_io/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.q_io;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
     Button loginBtn;
-    String userEmail = "";
-    String userPassword = "";
     Context ctx;
+    boolean isEmailValid, isPasswordValid;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.peoples_queue);
+        getSupportActionBar().setTitle("queue.io");
         setContentView(R.layout.activity_main);
         ctx = this;
         email = findViewById(R.id.email);
@@ -23,7 +31,33 @@ public class MainActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(v -> {
             Intent intent = new Intent(ctx, HomePageActivity.class);
+            SetValidation();
             startActivity(intent);
         });
+    }
+    public void SetValidation() {
+        // Validation email
+        if (email.getText().toString().isEmpty()) {
+            email.setError(getResources().getString(R.string.email_error));
+            isEmailValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+            email.setError(getResources().getString(R.string.error_invalid_email));
+            isEmailValid = false;
+        } else {
+            isEmailValid = true;
+        }
+        // Validation password
+        if (password.getText().toString().isEmpty()) {
+            password.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (password.getText().length() < 6) {
+            password.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else {
+            isPasswordValid = true;
+        }
+        if (isEmailValid && isPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Avec succès", Toast.LENGTH_SHORT).show();
+        }
     }
 }
