e1a7734fc8988eef1a7c4b63ecefb5de6dd20553
Dmytro Lunhu <dlunhu@isi-mtl.com>
2020-11-19 21:13:09 -0500

    Traitement des boutons. Début

--- mobile/q_io/app/src/main/java/com/example/q_io/MainActivity.java
+++ mobile/q_io/app/src/main/java/com/example/q_io/MainActivity.java
@@ -1,26 +1,15 @@
 package com.example.q_io;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-import java.text.DateFormat;
-import java.util.Calendar;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
@@ -31,28 +20,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Definition de logo
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.peoples_queue);
         getSupportActionBar().setTitle("queue.io");
         setContentView(R.layout.activity_main);
+        // Variables
         ctx = this;
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
         String user_email = "maria@gmail.com";
         String user_password = "maria";
         loginBtn = findViewById(R.id.login_btn_id);
+        // Passer à la page (terminal) d'employé
         loginBtn.setOnClickListener(v -> {
             Intent intent = new Intent(ctx, HomePageActivity.class);
 //            if (email.getText().toString().equals(user_email) && password.getText().toString().equals(user_password))
 //            {
-                startActivity(intent);
+            startActivity(intent);
 //            } else {
 //                SetValidation();
 //            }
         });
     }
     public void SetValidation() {
-        // Validation d'email
+        // Validation de courriel
         if (email.getText().toString().isEmpty()) {
             email.setError(getResources().getString(R.string.email_error));
             isEmailValid = false;
@@ -62,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             isEmailValid = true;
         }
-        // Validation de password
+        // Validation de mot de passe
         if (password.getText().toString().isEmpty()) {
             password.setError(getResources().getString(R.string.password_error));
             isPasswordValid = false;
@@ -76,8 +68,4 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Succès :)", Toast.LENGTH_SHORT).show();
         }
     }
-
-
-    //Appel API
-
 }
