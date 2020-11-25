ff699684d5f7cb804734ed0f91359827ecf546da
Isabel Streich <istreich@isi-mtl.com>
2020-11-25 12:53:47 -0500

    API put et get restant

--- server.js
+++ server.js
@@ -16,23 +16,30 @@ const HTTP_OK = 200
 const CONTENT_TYPE_JSON = 'application/json'
 const CONTENT_TYPE_HTML = 'text/html'
 
+//GET
 app.get('/commercetest/:commerce_id',controler.commercesss) //obtenir un commerce
 app.get('/commercetest/filtreId/:filtre_id',controler.commerceAvecFiltre)
 app.get('/commercetest',controler.commercesTous) //obtenir la liste de tous les commerces
 app.get('/commercetest/stats/:commerce_id',controler.commerceStat) //obtenir statistiques
 app.get('/test/login/:courriel/:mot_passe',controler.login)//Login
+app.get('/test/couleur',controler.couleur)//Login
+app.get('/test/filtre',controler.filtre)//Login
+
 // app.get('/commercetest/user/:role_id',controler.roleEmployee)//obtenir info employe-doesn't work
 // app.get('/commercetest/login/:courriel/:mot_passe',controler.login) //login
-//post
+
+//POST
 // app.post('/commercetest/creation_commerce', tracksControleur.ajout)//creation commerce//Formulaire d'Inscription
 app.post('/test/:nom/:adresse/:courriel/:mot_passe',controler_post.commerceInscription)//inscription
+app.post('/test/employecreation/:nom/:courriel/:mot_passe/:id_commerce',controler_post.employecreation)//inscription
+app.post('/test/servicesCreation/:nom_service/:duree_aprox',controler_post.servicesCreation)//inscription
 // app.post('/test/login/:courriel/:mot_passe',controler_post.login)//Login
 
-
+//PUT
 app.put('/commercetest/updateC',controlerUpdate.updateCouleur)
 
 
-app.get('/test/:roleid',controler.user)
+// app.get('/test/:roleid',controler.user)
 
 
 app.get('/commerce',function(request,response){
