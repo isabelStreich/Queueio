2c3dd55739f840a98fbe844d9699ea144b84f6ad
isivictorv <vvargasf@isi-mtl.com>
2020-11-25 12:52:57 -0500

    put commerce & horaire plus test

--- backend/manager/manager_update.js
+++ backend/manager/manager_update.js
@@ -20,12 +20,42 @@ const updateCouleur=class UpdateCouleur{
 }
 
 const updateHoraire=class UpdateHoraire{
-    static getData(){
+    static getData(ouvertureSemaine,fermetureSemaine,ouvertureWeekend,fermetureWeekend,id){
         let pgJsonResult=null
-        
+        return new Promise(resolve => {
+            dao.connect()
+            dao.query('UPDATE horaire set ouverture_semaine=$1, fermeture_semaine=$2, ouverture_weekend=$3,fermeture_weekend=$4 where id= $5', [ouvertureSemaine,fermetureSemaine,ouvertureWeekend,fermetureWeekend,id], (result) => {
+                if (result.rowCount > 0) {
+                    pgJsonResult = result.rows
+                } else {
+                    pgJsonResult = []
+                }
+                resolve(pgJsonResult)
+                dao.disconnect()
+            })
+        })
+    }
+}
+const updateCommerce=class UpdateCommerce{
+    static getData(nom,adress,courriel,mot_passe,id){
+        let pgJsonResult=null
+        return new Promise(resolve => {
+            dao.connect()
+            dao.query('UPDATE commerce set nom=$1, adresse=$2, courriel=$3,mot_passe=$4 where id= $5', [nom,adress,courriel,mot_passe,id], (result) => {
+                if (result.rowCount > 0) {
+                    pgJsonResult = result.rows
+                } else {
+                    pgJsonResult = []
+                }
+                resolve(pgJsonResult)
+                dao.disconnect()
+            })
+        })
     }
 }
 
 module.exports = {
-    updateCouleur
+    updateCouleur,
+    updateHoraire,
+    updateCommerce
 }
--- server.js
+++ server.js
@@ -32,7 +32,9 @@ app.post('/test/:nom/:adresse/:courriel/:mot_passe',controler_post.commerceInscr
 app.put('/commercetest/updateC',controlerUpdate.updateCouleur)
 
 
-app.get('/test/:roleid',controler.user)
+// app.get('/test/:roleid',controler.user)
+app.put('/update/horaire/:ouvertureSem,:fermetureSem,:ouvertureFinSem,:fermetureFinSem,:id',controlerUpdate.updateOuverture)
+app.put('/update/commerce/:nom,:adress,:courriel,:mot_passe,:id',controlerUpdate.updateCommerce)
 
 
 app.get('/commerce',function(request,response){
